da7d7d8ae7c5a575852876dc03948449618a04a2
Jacob Poirier <jacobapoirier9@gmail.com>
2022-10-08 12:33:37 -0400

    genre support

--- Spotify.ConsoleApp/Program.cs
+++ Spotify.ConsoleApp/Program.cs
@@ -7,14 +7,14 @@ namespace Spotify.ConsoleApp;
 internal class Program
 {
     private const string Url = "https://api.spotify.com/v1";
-    private const string Token = "BQABu33fIZO7AVBG6jCqPto1NDEMjbky6v57p78KFKHgW-TbTCCSVWAo5rbNmuthQHEgCepT_bxXbyzxbftejVoaufZmfw6vevEW3BIl0DqraT6JytUZs0mPNOksKHcEEUnEwuegErI6PO85ZwJPdokbVSRtInm6kOMgB42u_U_xOAi77CqEil9zJOTOibrLBO3WBO-J8_oF0c-0F5XYKIF2RqoixABVFcc8aTshTnO9SGccZSAvqJAfoGHAaK0pXr0gKSsXxoi1fcU5oBfg5WFcGtvS4W3l8ljKy_M65AAv8bq20ZEPjWUc2gv5ivYd";
+    private const string Token = "BQD8TTH1UY41TAPUBWdDAXP00dvGUh-pW3NIPt5mYSiiKhv8JQ9_DoaTay8QOIFEq0ZQKkQbU1y8jrLlbtplcN2HllfZM2z20sbbrKFmsBwPSn1c4cpSnS7NLJvBWCpmye09JyrYRiy1xoygGiO35rw8hdNThy5D8fi0mFhRHPO0wyREQRo4bFWrQfX-pZgJgqLeJeTZgcBxyAEojjm0fQgR62o5UUNXxkwfv1gshXKxoXur5VAp9a8eAFsTNvzvDRUi4xi8iG0bWhK9ecPRYAW6bryHqJzSsKcxwbahn0PjfgJjAUdXXdowy43RYi73";
 
     static void Main(string[] args)
     {
         // Jocko podcast show - 7irxBvxNqGYnUdFo1c2gMc
         var client = new SpotifyClient(Url);
 
-        var get = client.Get(new GetBrowseCategory { CategoryId = "toplists" }, Token);
+        var get = client.Get(new GetAvailableGenreSeeds(), Token);
     }
 }
 
---
+++ Spotify.Core/Model/Genre.cs
@@ -0,0 +1,25 @@
+﻿using Spotify.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify.Core.Model;
+
+/// <summary>
+/// Retrieve a list of available genres seed parameter values for recommendations.
+/// </summary>
+[Route("/recommendations/available-genre-seeds", Verb.Get)]
+public class GetAvailableGenreSeeds : IReturn<AvailableGenreSeeds>
+{
+
+}
+
+public class AvailableGenreSeeds
+{
+    /// <summary>
+    /// A set of genres
+    /// </summary>
+    public List<string>? Genres { get; set; }
+}
