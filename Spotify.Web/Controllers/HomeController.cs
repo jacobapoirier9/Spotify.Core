5e318d464a4abf7f1407fca994e3e44417178a63
Jacob Poirier <jacobapoirier9@gmail.com>
2022-10-10 20:59:29 -0400

    load template files

--- Spotify.Web/Controllers/HomeController.cs
+++ Spotify.Web/Controllers/HomeController.cs
@@ -1,30 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Spotify.Web.Models;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Spotify.Web.Controllers;
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController()
     {
-        _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    public IActionResult Index() => View();
 }
