05459e8da90c084bae29d968bf2b2605f39fb95b
Jacob Poirier <jacobapoirier9@gmail.com>
2022-10-19 21:37:23 -0400

    rewrite playlists view

--- Spotify.Web/wwwroot/js/site.js
+++ Spotify.Web/wwwroot/js/site.js
@@ -1,10 +1,5 @@
 ﻿/// <reference path="router.js" />
 /// <reference path="../lib/jquery/dist/jquery.js" />
-/// <reference path="../lib/jqgrid/ts/jqGrid.d.ts" />
-
-var helpers = {
-
-}
 
 var spotifyEnhancer = {
     home: {
@@ -12,7 +7,7 @@ var spotifyEnhancer = {
             init: function () {
                 $(".spotify-playlist").on("click", function () {
 
-                    window.location.assign(router.action("Home", "Playlists", { playlistId: $(this).attr("data-id") }))
+                    window.location.assign(router.action("Home", "Playlists", { playlistId: $(this).attr("data-playlist-id") }))
 
                 })
             }
